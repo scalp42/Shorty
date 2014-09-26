769970cafcc80db804f45178d09c98fe35a8f297
Corey Prophitt <prophitt.corey@gmail.com>
2014-09-26 02:30:41 -0700

    Added bundler support/gemfile.

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,8 @@
 require 'sinatra/base'
 
+# Require all universal gems plus environment specific gems
+Bundler.require(:default, ENV['RACK_ENV'].to_sym)
+
 class UploaderApp < Sinatra::Base
 
   get '/' do
